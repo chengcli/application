134e3efc7020ded0aff7980ff2ec2bd3c19c945b
Cheng Li <chengcli@umich.edu>
2022-12-23 17:42:00 -0500

    update cmake

--- cmake/setup_configure.cmake
+++ cmake/setup_configure.cmake
@@ -7,6 +7,13 @@ endif()
 # MPI flag
 option(UseMPI "Enable MPI" OFF)
 
+if (${UseMPI})
+  find_package(MPI REQUIRED)
+  set(EnableMPI MPI_PARALLEL)
+else()
+  set(EnableMPI NOT_MPI_PARALLEL)
+endif()
+
 if (CMAKE_BUILD_TYPE MATCHES "Debug")
   if (NOT "DEBUG" IN_LIST BUILD_TYPES)
     list(APPEND BUILD_TYPES "DEBUG")
